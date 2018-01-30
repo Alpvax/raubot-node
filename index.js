bca1d62250f4363cdda37b90b37b88d839f923c9
Alpvax <alpvax@gmail.com>
2018-01-30 17:21:54 +0000

    Begin commands, add translate to en

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 "use strict";
 
 const Telegraf = require("telegraf");
+const translate = require("google-translate-api");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) =>
-{
+bot.start((ctx) => {
   console.log("started:", ctx.from.id);
-  return ctx.reply("Welcome!");
+  return ctx.reply(`Welcome, ${ctx.from.first_name}!`);
 });
 
-bot.command("help", (ctx) => ctx.reply("Try send a sticker!"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there!"));
-bot.hears(/buy/i, (ctx) => ctx.reply("Buy-buy!"));
-bot.on("sticker", (ctx) => ctx.reply("👍"));
+bot.command("help", (ctx) => ctx.reply("Help coming soon..."));
+bot.command("echo", (ctx, next) => {
+  ctx.reply(ctx.message.text.substring(ctx.message.entities[0].length).trim());
+  if(ctx.message.text.match(/(-|=)+>$/)) {
+    next();
+  }
+});
 
+bot.on("text", (ctx, next) => {
+  translate(ctx.message.text, {to: "en"}).then(res => {
+    if(res.from.language.iso !== "en") {
+      ctx.reply(res.text, {reply_to_message_id: ctx.message.message_id});
+    }
+  }).catch(err => {
+    console.error(err);
+  });
+  next();
+});
+//bot.hears("hi", (ctx) => ctx.reply("Hey there!"));
+//bot.hears(/echo/i, (ctx) => ctx.reply("Did someone say \"echo\"?"));
+//bot.on("sticker", (ctx) => ctx.reply("👍"));
+
+console.log("Starting bot");
 bot.startPolling();
