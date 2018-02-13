8e81faf25546923aca6d0aa1a456c3dc99091aa0
Alpvax <alpvax@gmail.com>
2018-02-13 13:07:34 +0000

    Create AdvancedBot

--- index.js
+++ index.js
@@ -14,5 +14,39 @@ const database = firebase.database();
 const bot = new AdvancedBot(process.env.BOT_TOKEN, require("./package.json").version);
 bot.use(session(database.ref("telegram/sessions")));
 
+bot.defineSessionProperties((session, ctx) =>
+{
+  session.defineUserValue("__scenes");
+  session.defineUserValue("translateMyself");
+  session.defineUnsavedProperty("chatLangs", {
+    get()
+    {
+      return Object.entries(session.allUsers)
+        .filter(([uid, user]) => (ctx.chat.type == "private" || session.translateMyself || uid != ctx.from.id) && "userLangs" in user)
+        .map(([uid, user]) =>
+        {
+          return Object.entries(user.userLangs)
+            .filter(([k, v]) => v)
+            .map(([k, v]) => k);
+        })
+        .reduce((a, v) =>
+        {
+          v.forEach((l) =>
+          {
+            if(l in a)
+            {
+              a[l]++;
+            }
+            else
+            {
+              a[l] = 1;
+            }
+          });
+          return a;
+        }, {});
+    }
+  });
+});
+
 console.log("Starting bot");
 bot.startPolling();
